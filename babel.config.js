39615385e282a3221e18b1ab00d0855b85d0c6ad
BeataBarbara <skorusbeata@gmail.com>
2021-09-08 22:33:32 +0200

    deploy

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+    presets: ["@vue/cli-plugin-babel/preset"],
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/' + process.env.CI_vueIntro + '/' : '/'
 };
